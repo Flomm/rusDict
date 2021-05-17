60582f820fe1bdb739aedeee8035e94aa0fe7d9a
Florian Baranya <florian.baranya@gmail.com>
2021-05-17 21:11:08 +0200

    Rename helper function folder

--- react-app/src/components/upper/LeftSide/Keyboard.js
+++ react-app/src/components/upper/LeftSide/Keyboard.js
@@ -1,24 +1,24 @@
-import React from "react";
-import { abc } from "../../../resources/abc";
-import Key from "./Key";
+import React from 'react';
+import { abc } from '../../../supportFuncsAndObjects/abc';
+import Key from './Key';
 
 export default class Keyboard extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      case: "lower",
+      case: 'lower',
     };
     this.handleCase = this.handleCase.bind(this);
   }
 
   handleCase() {
-    if (this.state.case === "upper") {
+    if (this.state.case === 'upper') {
       this.setState({
-        case: "lower",
+        case: 'lower',
       });
     } else {
       this.setState({
-        case: "upper",
+        case: 'upper',
       });
     }
   }
@@ -26,12 +26,8 @@ export default class Keyboard extends React.Component {
   renderKeys(abc) {
     let keysArray = [];
     for (let i = 0; i < abc.length - 3; i++) {
-      let value = "";
-      if (
-        this.state.case === "lower"
-          ? (value = abc[i])
-          : (value = abc[i].toUpperCase())
-      )
+      let value = '';
+      if (this.state.case === 'lower' ? (value = abc[i]) : (value = abc[i].toUpperCase()))
         keysArray.push(
           <Key
             value={value}
@@ -56,16 +52,10 @@ export default class Keyboard extends React.Component {
       } else if (i === abc.length - 2) {
         onClick = this.props.onDelete;
       } else if (i === abc.length - 1) {
-        val = " ";
+        val = ' ';
       }
       keysArray.push(
-        <Key
-          value={abc[i]}
-          writeIn={val}
-          key={`${i}key`}
-          className={`key ${abc[i]}`}
-          onclick={onClick}
-        />
+        <Key value={abc[i]} writeIn={val} key={`${i}key`} className={`key ${abc[i]}`} onclick={onClick} />
       );
     }
     return keysArray;
