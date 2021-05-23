d5f8fbec6cee12814d1c3048e4aa0cbc84c36a18
Florian Baranya <florian.baranya@gmail.com>
2021-05-23 17:51:29 +0200

    Add wordlists and DB_filler

---
+++ DB_assets/DBfiller.py
@@ -0,0 +1,32 @@
+import os
+import pymongo
+
+
+def dict_reader(path, ord):
+    try:
+        file = open(path, encoding='utf-8')
+        line = file.readlines()
+        dict = []
+        for l in line:
+            ls = l[:len(l) - 1]
+            arr = ls.split(';')
+            row_obj = {ord[0]: arr[0], ord[1]: arr[1], 'pos': arr[2]}
+            if len(arr) > 3:
+                row_obj['gender'] = arr[3]
+            dict.append(row_obj)
+        file.close()
+        return dict
+    except:
+        print('Error while reading the file. Check if it exists.')
+
+
+def db_filler(dict_obj, dict_name):
+    try:
+        client = pymongo.MongoClient(os.environ.get('mongo'))
+        cluster = client['dict']
+        db = cluster[dict_name]
+        for article in dict_obj:
+            db.insert_one(article)
+        print('Success!')
+    except:
+        print('Wrong database name, or other error occured.')
