b07d1ef30078d655d079e1bb1f338fbc333f9968
Florian Baranya <florian.baranya@gmail.com>
2021-05-23 22:31:40 +0200

    Add 2 enpoints and error handling for server

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,12 +1,86 @@
-from flask import Flask, render_template
+import os
+import pymongo
+from flask import Flask, render_template, make_response
 
 app = Flask(__name__)
+client = pymongo.MongoClient(os.environ.get('dict_mongo_read'))
+cluster = client['dict']
+
+
+class APIError(Exception):
+    message = Exception.args
+    code = 404
+    pass
+
+
+def is_integer(n):
+    try:
+        float(n)
+    except ValueError:
+        return False
+    else:
+        return float(n).is_integer()
+
+
+def are_params_okay(word, limit):
+    if not word or not limit or not is_integer(limit):
+        return False
+    if int(limit) > 20:
+        return False
+    else:
+        return True
+
+
+@app.errorhandler(APIError)
+def handle_exception(err):
+    response = {"error": err.message}
+    return response, err.code
+
+
+@app.errorhandler(500)
+def handle_exception():
+    response = {
+        "error": "Hopsz, úgy tűnik valami gond van a szerverrel. Elnézést. Kérünk, próbálkozz később."}
+    return response, 500
+
+
+# @app.after_request
+# def after_request_func(data):
+#     response = make_response(data)
+#     response.headers['Content-Type'] = 'application/json'
+#     return response
 
 
 @app.route('/')
 def index():
-    return render_template("index.html", flask_token="Hello   world")
+    return render_template("index.html", flask_token="Hello world")
+
+
+@app.route('/api/RU/<word>/<limit>')
+def getRU(word, limit):
+    if not are_params_okay(word, limit):
+        raise APIError(
+            'Hopsz. Úgy tűnik rossz paramétereket adtál meg. Próbáld újra.')
+    response = list(cluster.RU.find(
+        {'RU': {'$regex': f'.*{word}.*'}}, {'_id': 0}).limit(int(limit)))
+    if len(response) < 1:
+        raise APIError(
+            'Hopsz. Sajnos ez a szó még nem szerepel az adatbázisunkban, vagy nem létezik.')
+    return {'result': response}
+
+
+@app.route('/api/HU/<word>/<limit>')
+def getHU(word, limit):
+    if not are_params_okay(word, limit):
+        raise APIError(
+            'Hopsz. Úgy tűnik rossz paramétereket adtál meg. Próbáld újra.')
+    response = list(cluster.HU.find(
+        {'HU': {'$regex': f'.*{word}.*'}}, {'_id': 0}).limit(int(limit)))
+    if len(response) < 1:
+        raise APIError(
+            'Hopsz. Sajnos ez a szó még nem szerepel az adatbázisunkban, vagy nem létezik.')
+    return {'result': response}
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
