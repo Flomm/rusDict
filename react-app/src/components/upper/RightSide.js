b900b9c4f432d0adb83e0bbc5242f54d8f219f26
Florian Baranya <florian.baranya@gmail.com>
2021-06-01 21:45:15 +0200

    Rename rightside to switch component

--- react-app/src/components/upper/RightSide.js
+++
@@ -1,7 +0,0 @@
-export const InfoPart = () => {
-  return (
-    <div className="info">
-      <h5>Jelmagyarázat</h5>
-    </div>
-  );
-};
--- react-app/src/components/upper/upperMain.js
+++ react-app/src/components/upper/upperMain.js
@@ -1,8 +1,7 @@
 import { useState } from 'react';
 import WordForm from './LeftSide/WordForm';
-// import { InfoPart } from './RightSide';
 import { useGlobState } from '../context';
-import { Switch } from './middle';
+import { Switch } from './switch';
 
 export const Upper = (props) => {
   const [pair, setPair] = useState('RUS-HU');
@@ -23,14 +22,11 @@ export const Upper = (props) => {
     <div className="main upper">
       <div className="upper holder">
         <div className="upper-left">
-          <WordForm pair={pair} onsubmit={props.onsubmit} />
+          <WordForm onsubmit={props.onsubmit} />
         </div>
         <div>
           <Switch pair={pair} handleChange={handleChange} />
         </div>
-        {/* <div className="upper-right">
-          <InfoPart />
-        </div> */}
       </div>
     </div>
   );
